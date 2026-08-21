d6d788ef534d5e2045c51d55bb17a6dca65b417e
REAPER PROJECT <cxllme14zx@gmail.com>
2026-08-21 21:03:23 +0700

    Update thai.lua

--- thai.lua
+++ thai.lua
@@ -37,8 +37,8 @@ local Camera = workspace.CurrentCamera
 local lp = LocalPlayer
 
 local Window = Fluent:CreateWindow({
-Title = "Reaper Hub",
-SubTitle = "Violence District",
+Title = "Reaper Hub | Violence District [BETA]",
+SubTitle = "",
 TabWidth = 160,
 Size = UDim2.fromOffset(520, 360),
 Theme = "ExtremeReaper",
