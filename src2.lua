6685525c33e23f53b18578a56603cccba298192c
REAPER PROJECT <cxllme14zx@gmail.com>
2026-09-12 21:00:05 +0700

    Update comments in src2.lua

--- src2.lua
+++ src2.lua
@@ -1,4 +1,4 @@
--- 4
+-- 5
 local Load = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Libwtf/refs/heads/main/libload2.lua"))() 
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/SaveManager.lua"))()
