b57ebbc2ed347f90d4c770f484d91982edc54ece
REAPER PROJECT <cxllme14zx@gmail.com>
2026-09-15 21:50:58 +0700

    Update src2.lua

--- src2.lua
+++ src2.lua
@@ -1,11 +1,10 @@
--- 2
+-- 3
 --local Load = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Libwtf/refs/heads/main/libload2.lua"))() 
---local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/main.lua"))()
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/main.lua"))()
 --local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/SaveManager.lua"))()
 --local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2sxqz/Advanced/refs/heads/main/gui/InterfaceManager.lua"))()
 
 local Load = loadstring(game:HttpGet("https://reaperxsecurity.vercel.app/38Eutmo2vzGrhZp1RL"))()
-local Fluent = loadstring(game:HttpGet("https://reaperxsecurity.vercel.app/FC8ea07BzRHAUvQzHy"))()
 local SaveManager = loadstring(game:HttpGet("https://reaperxsecurity.vercel.app/yQiFt3LftNb3xTa2Go"))()
 local InterfaceManager = loadstring(game:HttpGet("https://reaperxsecurity.vercel.app/cCocd82HJSFgEKkXdv"))()
 
